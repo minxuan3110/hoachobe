3c40d6466815ef6b221ae2401937ee59fe80f299
minxuan3110 <atx.ytbpre@gmail.com>
2025-06-15 09:30:26 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Hello. Chị có cái nì cho elm nò`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
