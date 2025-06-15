d7c04366dcc8886ef2d22c1b7db8074f29eab352
minxuan3110 <atx.ytbpre@gmail.com>
2025-06-15 09:34:16 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Hello. Chị có cái nì cho elm nò`.split('')
+const text = `Cốc Cốc. Chị có cái nì cho Ang nò`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
